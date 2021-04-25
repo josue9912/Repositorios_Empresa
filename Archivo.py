83ece6d061854582b3962beab3dfa5113d884873
Josue <josue.santana@birdlandco.com>
2021-04-25 16:51:30 +0100

    Primera version

---
+++ Archivo.py
@@ -0,0 +1,40 @@
+import os
+# coding: utf-8
+import shutil
+
+thisdir = "/Users/josuesantanagalvan/Desktop/Carpeta/Carpeta1/Carpeta2"
+
+for r, d, f in os.walk(thisdir): #Creo las carpetas
+
+        if 'CarpetaFinal' in r:
+            print(r)
+            try:
+
+                r1 = os.path.join(r, 'main')
+                os.mkdir(r1)
+                r2 = os.path.join(r, 'main','V01')
+                os.mkdir(r2)
+
+            except FileExistsError:
+                print("Ya existen las carpetas main y V01")
+                break
+
+for r, d, f in os.walk(thisdir): #MODIFICO EL NOMBRE DE LOS ARCHIVOS
+    for file in f:
+
+        if file.endswith(".py"):
+
+            print(file)
+            ruta_antigua = os.path.join(r,file)
+            print(ruta_antigua)
+
+            nuevo_fichero = file.replace('jercicio','1')
+            ruta_nueva = os.path.join(r,nuevo_fichero)
+            print(ruta_nueva)
+            os.rename(ruta_antigua,ruta_nueva)
+
+            try:
+
+                shutil.move(ruta_nueva,r+"/main/V01")
+            except:
+                print("Error")
