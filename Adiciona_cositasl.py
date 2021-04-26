91534f81c5cd8ea80fe3853106c733115eb82e0f
Josue <josue.santana@birdlandco.com>
2021-04-26 13:35:54 +0100

    Sin solucionar

--- Adiciona_cositasl.py
+++ Adiciona_cositasl.py
@@ -5,7 +5,7 @@ import shutil
 thisdir = '/Users/josuesantanagalvan/Desktop/Carpeta_Prueba/Carpeta1/Carpeta2'
 
 
-'''for r, d, f in os.walk(thisdir):
+for r, d, f in os.walk(thisdir):
 
         for direcotorios in d:
             if "VFX" in direcotorios:
@@ -13,7 +13,7 @@ thisdir = '/Users/josuesantanagalvan/Desktop/Carpeta_Prueba/Carpeta1/Carpeta2'
                     r1 = os.path.join(r, 'main')
                     os.mkdir(r1)
                 except:
-                    print("Ya existen las carpetas main")'''
+                    print("Ya existen las carpetas main")
 
 for r, d, f in os.walk(thisdir):
     for file in f:
