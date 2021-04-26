76195d7f4c194581986fdd070faf6af60639d1aa
Josue <josue.santana@birdlandco.com>
2021-04-26 20:23:17 +0100

    Solucionado-Josue

--- Adicional_Archivos.py
+++ Adicional_Archivos.py
@@ -4,11 +4,10 @@ import shutil
 
 thisdir = '/Users/josuesantanagalvan/Desktop/Carpeta_Prueba/Carpeta1/Carpeta2'
 
-
 for r, d, f in os.walk(thisdir):
 
-        for direcotorios in d:
-            if "VFX" in direcotorios:
+        for directorios in d:
+            if "VFX" in directorios:
                 try:
                     r1 = os.path.join(r, 'main')
                     os.mkdir(r1)
@@ -22,34 +21,28 @@ for r, d, f in os.walk(thisdir):
             old_root = os.path.join(r, file)
             new_file = file.replace('jercicio', '1')
             new_root = os.path.join(r, new_file)
-
             os.rename(old_root, new_root)
 
-            nueva = r.replace("VFX","main")
-            print(nueva)
-
-
-            #shutil.move(new_root,nueva)
-
-
-
-
-
-
-
-
-'''
-            try:
-                pass
-               # shutil.move(new_root,r2)
-            except:
-                print("WTF BRO")
-
-
 for r, d, f in os.walk(thisdir):
-    for direcotorios in d:
-        if "VFX" in direcotorios:
-            print(r)'''
+
+    for file in f:
+
+        if file.endswith(".py"):
+            ruta_py = os.path.join(r,file)
+            print(ruta_py)
+
+            ruta_main = r.replace("VFX","main")
+            print(ruta_main)
+
+            try:
+                shutil.move(ruta_py,ruta_main)
+            except:
+                print("Error")
+
+
+
+
+
 
 
 
