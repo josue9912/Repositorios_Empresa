340d247b0f02b54518a3bc1ef0bdeb1759d3d14a
Josué <josue.santana@birdlandco.com>
2021-04-29 13:11:21 +0100

    Version-2

--- find.py
+++ find.py
@@ -24,8 +24,26 @@ def storeFiles(path):
     dictionary={"Path":tuple_path,"File":tuple_file,"Extension":tuple_extension}
     print(dictionary)
 
+def get_all_file(dir_path, get_current = False):
+    from pathlib import Path
+    list = []
 
-print(storeFiles(path))
+    files = [f.name for f in Path(dir_path).rglob("*.*")]
+    ext = [f.suffix for f in Path(dir_path).rglob("*.*")]
+    r = [f.anchor for f in Path(dir_path).rglob("*.*")]
+
+    for i in files:
+        root = os.path.join(path,i)
+        list.append(root)
+
+    dictionary={"Files":files,"Path":list,"Ext":ext}
+
+    return dictionary
+
+
+
+print(get_all_file(path))
+
 
 
 
